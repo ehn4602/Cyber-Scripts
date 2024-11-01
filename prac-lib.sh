c64107d91cd2049ccb886578c730bcc561f16573
ehn4602 <90865646+ehn4602@users.noreply.github.com>
2024-11-01 06:46:52 -0700

    updated installer

--- prac-lib.sh
+++ prac-lib.sh
@@ -6,10 +6,18 @@
 wget https://github.com/samratashok/ADModule/archive/refs/heads/master.zip -O admodule.zip
 wget https://raw.githubusercontent.com/PowerShellMafia/PowerSploit/master/Recon/PowerView.ps1 -O powerview.ps1
 
-# Downloads custom Powershell installers
+# Downloads go-windapsearch
+wget "https://github.com/ropnop/go-windapsearch/releases/download/v0.3.0/windapsearch-linux-amd64" -O windapsearch
+chmod +x windapsearch
 
+# Downloads custom repo items
 wget https://raw.githubusercontent.com/ehn4602/Cyber-Scripts/refs/heads/main/PowerView-Install.ps1 -O powerview-install.ps1
 wget https://raw.githubusercontent.com/ehn4602/Cyber-Scripts/refs/heads/main/ADM-Install.ps1 -O adm-install.ps1
+wget https://raw.githubusercontent.com/ehn4602/Cyber-Scripts/refs/heads/main/alt.txt
+wget https://raw.githubusercontent.com/ehn4602/Cyber-Scripts/refs/heads/main/windap-prac.sh
+chmod +x windap-prac.sh
+wget https://raw.githubusercontent.com/ehn4602/Cyber-Scripts/refs/heads/main/ldap-prac.sh
+chmod +x ldap-prac.sh
 
-#Starts http server here
+# Starts up http server
 python3 -m http.server 8080
---
+++ readme.md
@@ -0,0 +1,5 @@
+Files for practical 2 
+intended for virtual environment
+
+Command to get the installer, then run prac-lib.sh to setup everything else 
+wget https://raw.githubusercontent.com/ehn4602/Cyber-Scripts/refs/heads/main/prac-lib.sh
